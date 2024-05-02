a350f1a52cb15f185c7af588563e86f0abc1bfe1
Britt Treece <britt@spreedly.com>
2024-05-02 12:52:16 -0500

    Remove non-functional performance tests...
    Also ran a `rails app:upgrade`

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
-require_relative '../env'
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -13,7 +12,6 @@ require "action_controller/railtie"
 # require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -25,16 +23,17 @@ module PhasersSample
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
 
-    config.encoding = "utf-8"
-    config.autoload_paths += %W(#{config.root}/lib)
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
-    if ENV['APP_HOST']
-      config.hosts << ENV['APP_HOST']
-    end
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
